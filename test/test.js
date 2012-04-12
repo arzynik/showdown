0352a3bc122ef532dbb99981d99030e8ff7c17c2
arzynik <github@arzynik.com>
2012-04-12 12:23:41 -0700

    added diff for tests

--- test/test.js
+++ test/test.js
@@ -12,22 +12,18 @@ var
 	reset = '\033[0m',
 	bold = '\033[1;37m',
 	red = '\033[1;31m',
-	green  = '\033[1;32m';
+	green  = '\033[1;32m',
+	exec = require('child_process').exec;
 
-var	readfile = function(file) {
-	fs.readFile(testDir + '/Tests/' + file, 'ascii', function(err, data) {
-		if (err) {
-			console.error(red + "Could not open file: %s" + reset, err);
-			process.exit(1);
-		}
-		return data;
-	});
-}
 
 var showdown = new sd.Showdown.converter({
-	'github_flavouring': true,
-	'tables': true
+	'github_flavouring': false,
+	'tables': false
 });
+
+var shellEscape = function(arg) {
+	return arg.replace(/( |\(|\))/g,'\\$1')
+}
 
 fs.readdir(testDir + '/Tests/',function(err, files){
 	if (err) throw err;
@@ -61,14 +57,22 @@ fs.readdir(testDir + '/Tests/',function(err, files){
 				if (result == html) {
 					console.log(filename + chars + '[' + green + ' YES ' + reset + ']');
 				} else {
-/*
-console.log(green + html);
-console.log(red + result);
+					console.log(bold + filename + reset + chars +  '[' + red + ' NO  ' + reset + ']');
+					
 
-console.log(reset);
-process.exit(1);
-*/
-					console.log(bold + filename + reset + chars +  '[' + red + ' NO  ' + reset + ']');		
+					fs.writeFile(testDir + '/Tests/' + filename + '.result.html', result, function(err) {
+						if (err) {
+							console.log(err);
+					    }
+					    var cmd = 'diff ' + testDir + '/Tests/' + shellEscape(filename) + '.html ' + testDir + '/Tests/' + shellEscape(filename) + '.result.html';
+						exec(cmd, function (error, username) {
+						    console.log(bold + filename + reset);
+						    console.log(username + "\n");
+						    fs.unlink(testDir + '/Tests/' + filename + '.result.html', result);
+						});
+
+					    
+					});		
 				}
 	
 			});
